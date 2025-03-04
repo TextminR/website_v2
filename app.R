05c21cc457e30161be14f542dcfc9b42473528be
thomaslaber <tlaber@student.tgn>
2025-03-04 13:03:54 +0100

    Implemented Intertopic plot

--- app.R
+++ app.R
@@ -5,6 +5,7 @@ library(jsonlite)
 library(highcharter)
 library(dplyr)
 library(leaflet)
+library(ggplot2)
 
 IP <- "localhost"
 
@@ -22,6 +23,11 @@ ui <- navbarPage("TextminR",
                               column(3, img(src = "images/TVLogoTransparent.png", height = "92px", width = "276px"))
                             )
                           ),
+                 ),
+                 tabPanel("Topic Ansicht",
+                          mainPanel(
+                            highchartOutput("interTopic")
+                          )
                           ),
                  tabPanel("Themenrelevanz in der Literatur",
                           sidebarLayout(
@@ -73,6 +79,34 @@ ui <- navbarPage("TextminR",
                             ),
                             mainPanel(
                               leafletOutput("heatmap")
+                            )
+                          )
+                 ),
+                 tabPanel("Specific Document",
+                          sidebarLayout(
+                            sidebarPanel(
+                              div(
+                                class="custom_sidebar",
+                                textInput("such_string", "Suchfeld", placeholder = "Suchen Sie ein Werk")
+                              ),
+                              div(
+                                style = "overflow-y: scroll; max-height: 400px;",
+                                uiOutput("documents_list"),
+                                div(
+                                  style = "position: absolute; bottom: -10px; left: 10px; width: calc(100% - 20px);",
+                                  actionButton(
+                                    inputId = "plot_button",
+                                    label = "Topics anzeigen",
+                                    style = "margin-bottom: 10px;
+                                    padding: 5px 15px; background-color: #007BFF; color: white;
+                                    border: none; border-radius: 5px; cursor: pointer; display: block;
+                                    width: 100%; font-size: 14px;"
+                                  )
+                                )
+                              )
+                            ),
+                            mainPanel(
+                              verbatimTextOutput("document_content")
                             )
                           )
                  )
@@ -287,7 +321,10 @@ server <- function(input, output, session) {
     }
   })
   
-
+  
+  flatten_topics <- function(topics) {
+    sapply(topics, function(x) paste(names(x), x, sep = ":", collapse = ", "))
+  }
   
   # Plot-Funktion für zweiten Tab
   observeEvent(input$plot_btn2, {
@@ -301,6 +338,8 @@ server <- function(input, output, session) {
     res <- httr::GET(
       url = sprintf("http://%s:8000/heatmap?startdate=%s&enddate=%s&topic=%s", IP, startdate, enddate, selected_topics2()),
     )
+    
+    print(res)
     
     
     if (httr::status_code(res) == 200) {
@@ -324,14 +363,23 @@ server <- function(input, output, session) {
         return()
       }
       
+      print(data)
+      
+      safe_topics <- lapply(data$topics, function(x) {
+        if (is.null(x) || length(x) == 0) return(NA) # Leere Topics mit NA füllen
+        paste(names(x), x, sep = ":", collapse = ", ")
+      })
+      
       data_df <- data.frame(
         title = data$title,
         longitude = data$longitude,
         latitude = data$latitude,
         sentiment = data$sentiment,
-        topics = data$topics,
+        id = data$id,
         stringsAsFactors = FALSE
       )
+      
+      print(data_df)
       
       plot_data2(data_df)
     }
@@ -449,6 +497,196 @@ server <- function(input, output, session) {
         )
     }
     return(hc)
+  })
+  
+  df <- reactiveVal()
+  document_content <- reactiveVal()
+  content_id <- reactiveVal()
+  
+  observe({
+    IP <- IP
+    Pfad <- "/documents"
+    url <- sprintf("http://%s:8000%s", IP, Pfad)
+    
+    res <- httr::GET(url)
+    
+    status <- httr::status_code(res)
+    
+    if (status == 200) {
+      documents <- httr::content(res, as = "parsed")
+      
+      df(data.frame(
+        id = names(documents),
+        titel = unlist(documents, use.names = FALSE),
+        stringsAsFactors = FALSE
+      ))
+    } else {
+      error_message <- paste("Fehler beim API-Aufruf! HTTP Status:", status)
+      
+    }
+  })
+  
+  filtered_documents <- reactive({
+    req(df())
+    if (!"titel" %in% colnames(df())) {
+      stop("Die Spalte 'titel' wurde nicht gefunden.")
+    }
+    
+    if (input$such_string != "") {
+      df() %>%
+        filter(grepl(input$such_string, titel, ignore.case = TRUE))
+    } else {
+      df()
+    }
+  })
+  
+  output$documents_list <- renderUI({
+    req(filtered_documents())
+    
+    if (nrow(filtered_documents()) == 0) {
+      tags$p("Keine Dokumente gefunden.")
+    } else {
+      tags$ul(
+        lapply(1:nrow(filtered_documents()), function(i) {
+          documents_name <- filtered_documents()$titel[i]
+          document_id <- filtered_documents()$id[i]
+          actionLink(
+            inputId = paste0("document_", document_id), 
+            label = documents_name, 
+            style = "display: block; 
+              margin-left: -38px;
+              font-size: 16px;
+              color: #000000;
+              border-bottom: 1px solid #000000;
+              margin-bottom: 10px;
+              transition: color 0.3s ease;")
+        })
+      )
+    }
+  })
+  
+  observe({
+    req(filtered_documents())
+    
+    lapply(1:nrow(filtered_documents()), function(i) {
+      documents_id <- filtered_documents()$id[i]
+      action_link_id <- paste0("document_", documents_id)
+      
+      observeEvent(input[[action_link_id]], {
+        content_id(documents_id)
+        IP <- IP
+        url <- sprintf("http://%s:8000/document/content/%s?n=30", IP, documents_id)
+        
+        res <- httr::GET(url)
+        
+        if (httr::status_code(res) == 200) {
+          documents_data <- httr::content(res, as = "parsed")
+          
+          df_inhalt <- data.frame(
+            inhalt = documents_data,
+            stringsAsFactors = FALSE
+          )
+          
+          document_content(paste(df_inhalt, collapse = "\n"))
+        } else {
+          document_content("Fehler beim Laden des Dokuments.")
+        }
+      })
+    })
+  })
+  
+  output$document_content <- renderText({
+    if (is.null(document_content())) {
+      return("Noch kein Dokument ausgewählt oder kein Inhalt verfügbar.")
+    } else {
+      req(document_content())
+      document_content()
+    }
+  })
+  
+  observeEvent(input$plot_button, {
+    showModal(
+      modalDialog(
+        title = "Topics",
+        plotOutput("modal_plot"),
+        size = "l",
+        easyClose = TRUE,
+        footer = modalButton("Schließen")
+      )
+    )
+  })
+  
+  output$modal_plot <- renderPlot({
+    req(content_id())
+    
+    IP <- IP
+    url <- sprintf("http://%s:8000/document/topics/%s", IP, content_id())
+    
+    res <- httr::GET(url)
+    
+    doc_topics <- data.frame(doc_TopicName = character(), doc_TopicWert = numeric())
+    
+    if (httr::status_code(res) == 200) {
+      doc_topics_data <- httr::content(res, as = "text")
+      
+      doc_topics_data <- gsub('\\"', '"', doc_topics_data)
+      
+      doc_topics_data <- jsonlite::fromJSON(doc_topics_data)
+      
+      if (!is.null(doc_topics_data)) {
+        doc_topics <- data.frame(
+          doc_TopicName = names(doc_topics_data),
+          doc_TopicWert = unlist(doc_topics_data, use.names = FALSE),
+          stringsAsFactors = FALSE
+        )
+      } else {
+        showNotification("Keine Daten verfügbar für die Topics des Dokuments.", type = "warning")
+      }
+    } else {
+      showNotification(sprintf("Fehler beim API-Aufruf: HTTP %d", httr::status_code(res)), type = "error")
+      return(NULL)
+    }
+    
+    ggplot(doc_topics, aes(x = doc_TopicWert, y = reorder(doc_TopicName, doc_TopicWert), fill = doc_TopicWert)) +
+      geom_bar(stat = "identity", color = "black") +
+      scale_fill_gradient(low = "#03a1fc", high = "#1803fc") +
+      labs(
+        title = sprintf("Topics des ausgewählten Werkes"),
+        x = "",
+        y = ""
+      ) +
+      theme_minimal(base_size = 14) +
+      theme(
+        plot.title = element_text(hjust = 0.5, face = "bold", size = 16),
+        axis.title.x = element_text(margin = margin(t = 10)),
+        axis.title.y = element_text(margin = margin(r = 10)),
+        axis.text.y = element_text(size = 12),
+        axis.text.x = element_text(size = 12),
+        legend.position = "none"
+      )
+  })
+  
+  output$interTopic <- renderHighchart({
+    url <- sprintf("http://%s:8000/topicplot", IP)
+    res <- httr::GET(url)
+    
+    
+    content <- content(res, as = "text", encoding = "UTF-8")
+    json_data <- fromJSON(content)
+    
+    df <- as.data.frame(json_data)
+    
+    highchart() %>%
+      hc_chart(type = "scatter") %>%
+      hc_add_series(data = df, type = "bubble",
+                    hcaes(x = x, y = y, size = size, name = topic),
+                    dataLabels = list(enabled = TRUE, format = "{point.name}", showInLegend = FALSE)) %>%
+      hc_xAxis(title = list(text = "X-Koordinate")) %>%
+      hc_yAxis(title = list(text = "Y-Koordinate")) %>%
+      hc_title(text = "Topic Streudiagramm") %>%
+      hc_tooltip(pointFormat = "Topic: {point.name}") %>%
+      hc_legend(enabled = FALSE)
+    
   })
 }
 
